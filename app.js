52ae78d20766527a472dc736e22dfb3312aa9319
Adriano Junior Vidal <adriano.jv@hotmail.com>
2022-11-03 02:57:30 -0300

    02- Desenvolvimento do front-end do projeto

---
+++ app.js
@@ -0,0 +1,47 @@
+//! Módulos necessários e configurações
+//* Express
+const express = require('express');
+const app = express();
+
+//* Body-Parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());                          // to suport JSON-encoded bodies
+app.use(bodyParser.urlencoded({extended: true}))     // to suport URL-encoded bodies
+
+//* Path   
+const path = require('path');             
+
+//* EJS
+const ejs = require('ejs');                  
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views/pages'));
+
+//-------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------
+//! Servidor
+app.listen(3000, ()=>{
+    console.log("Servidor funcionando!")
+})
+
+
+//-------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------
+//! Rotas
+//* Pages
+//Home
+app.get('/', (req,res)=>{
+    //Rota para query de pesquisa =busca
+    if(req.query.busca == null){                   // Se não tiver buscando nada, fornece a Home
+        res.render('home', {});                     // Renderização da Home + dados 
+    } else{
+        res.send('Você busco: '+req.query.busca)
+    }
+})
+    
+//* Slug - URL da notícia
+//Home
+app.get('/:slug', (req, res)=>{
+    res.send(req.params.slug)
+})
